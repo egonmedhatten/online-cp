aacfbb2bb05b1967f59e8a1b289aa397281c141a
Johan Hallberg Szabadvary <johan.hallberg@algorithma.se>
2024-07-09 14:36:19 +0200

    Added knn

--- CRR.py
+++ CRR.py
@@ -2,6 +2,8 @@ import numpy as np
 import time
 import warnings
 from scipy.optimize import minimize_scalar, minimize, Bounds
+from scipy.spatial.distance import pdist, cdist, squareform
+
 
 MACHINE_EPSILON = np.finfo(np.float64).eps
 
@@ -127,7 +129,6 @@ class ConformalRidgeRegressor(ConformalRegressor):
         self.autotune = autotune
 
         self.verbose = verbose
-
         self.rnd_gen = np.random.default_rng(rnd_state)
 
 
@@ -698,9 +699,230 @@ class KernelConformalRidgeRegressor(ConformalRegressor):
         return p_y
 
 
+class ConformalNearestNeighboursRegressor(ConformalRegressor):
+
+    def __init__(self, k, distance='euclidean', kernel=None, distance_func=None, warnings=True, verbose=0, rnd_state=2024):
+        
+        self.k = k
+        self.distance = distance
+        if distance_func is None:
+            self.distance_func = self._standard_distance_func
+        else:
+            self.distance_func = distance_func
+            self.distance = 'custom'
+
+        self.X = None
+        self.y = None
+
+        self.verbose = verbose
+        self.rnd_gen = np.random.default_rng(rnd_state)
+
+        self.warnings = warnings
+
+
+    def _standard_distance_func(self, X, y=None):
+        '''
+        By default we use scipy to compute distances
+        '''
+        X = np.atleast_2d(X)
+        if y is None:
+            dists = squareform(pdist(X, metric=self.distance))
+        else:
+            y = np.atleast_2d(y)
+            dists = cdist(X, y, metric=self.distance)
+        return dists
+    
+
+    def learn_initial_training_set(self, X, y):
+        self.X = X
+        self.y = y
+        self.D = self.distance_func(X)
+
+    @staticmethod
+    def update_distance_matrix(D, d):
+        return np.block([[D, d], [d.T, np.array([0])]])
+
+    def learn_one(self, x, y, precomputed=None):
+        '''
+        precomputed is a dictionary
+        {
+            'X': X,
+            'D': D,
+            'A': A,
+            'B': B
+        }
+        '''
+        # Learn label y
+        if self.y is None:
+            self.y = np.array([y])
+        else:
+            self.y = np.append(self.y, y)
+
+        if precomputed is None:
+            # Learn object x
+            if self.X is None:
+                self.X = x.reshape(1,-1)
+                self.D = self.distance_func(self.X)
+            else:
+                d = self.distance_func(self.X, x)
+                self.D = self.update_distance_matrix(self.D, d)
+                self.X = np.append(self.X, x.reshape(1, -1), axis=0)
+        else:
+            self.X = precomputed['X']
+            self.D = precomputed['D']
+    
+
+    def predict(self, x, epsilon=0.1, bounds='both', return_update=False, debug_time=False):
+
+        def build_precomputed(X, D, A, B):
+            computed = {
+                'X': X,
+                'D': D,
+                'A': A,
+                'B': B
+            }
+            return computed
+        
+        if self.X is not None:
+
+            # Temporarily update distance matrix
+            tic = time.time()
+            d = self.distance_func(self.X, x)
+            D = self.update_distance_matrix(self.D, d)
+            toc_update_D = time.time() - tic
+
+            tic = time.time()
+            # Add row to X matrix
+            X = np.append(self.X, x.reshape(1, -1), axis=0)
+            toc_add_row = time.time() - tic
+            n = X.shape[0]
+
+            # Check that the significance level is not too small. If it is, return infinite prediction interval
+            if bounds=='both':
+                if not (epsilon >= 2/n):
+                    if self.warnings:
+                        warnings.warn(f'Significance level epsilon is too small for training set. Need at least {int(np.ceil(2/epsilon))} examples. Increase or add more examples')
+                    if return_update:
+                        return (-np.inf, np.inf), build_precomputed(X, D, None, None)
+                    else: 
+                        return (-np.inf, np.inf)
+            else: 
+                if not (epsilon >= 1/n):
+                    if self.warnings:
+                        warnings.warn(f'Significance level epsilon is too small for training set. Need at least {int(np.ceil(1/epsilon))} examples. Increase or add more examples')
+                    if return_update:
+                        return (-np.inf, np.inf), build_precomputed(X, D, None, None)
+                    else: 
+                        return (-np.inf, np.inf)
+            
+            tic = time.time()
+            A, B = self.compute_A_and_B(D, self.y, self.k)
+            toc_nc = time.time() - tic
+
+            tic = time.time()
+            l_dic, u_dic = self._vectorised_l_and_u(A, B)
+            toc_dics = time.time() - tic
+
+            if bounds=='both':
+                lower = self._get_lower(l_dic=l_dic, epsilon=epsilon/2, n=n)
+                upper = self._get_upper(u_dic=u_dic, epsilon=epsilon/2, n=n)
+            elif bounds=='lower':
+                lower = self._get_lower(l_dic=l_dic, epsilon=epsilon, n=n)
+                upper = np.inf
+            elif bounds=='upper':
+                lower = -np.inf
+                upper = self._get_upper(u_dic=u_dic, epsilon=epsilon, n=n)
+            else: 
+                raise Exception
+
+            if debug_time:
+                print(f'Add row: {toc_add_row}')
+                print(f'Update D: {toc_update_D}')
+                print(f'NC scores: {toc_nc}')
+                print(f'l and u: {toc_dics}')
+                print()
+        else:
+            # With just one object, and no label, we cannot predict any meaningful interval
+            lower = -np.inf
+            upper = np.inf
+            X = x.reshape(1,-1)
+            D = self.distance_func(X)
+            A = None
+            B = None
+        
+        if return_update:
+            return (lower, upper), build_precomputed(X, D, A, B)
+        else:
+            return (lower, upper)
+
+    @staticmethod
+    def compute_A_and_B(D, y, k):
+        y = np.append(y, 0)
+        n = D.shape[0] - 1 
+        A = np.zeros(n + 1)
+        B = np.zeros(n + 1)
+
+        k_nearest = D.argsort(axis=0)[1:k+1]
+
+        for i, col in enumerate(k_nearest.T): # Transpose to iterate over columns
+            if i < n:
+                A[i] = y[i] - y[col].mean()
+                if n in col:
+                    B[i] = -1/k
+                else:
+                    B[i] = 0
+            else:
+                A[i] = -y[col].mean()
+                B[i] = 1
+        return A, B
+    
+
+    def compute_smoothed_p_value(self, x, y, precomputed=None):
+        '''
+        Computes the smoothed p-value of the example (x, y).
+        Smoothed p-values can be used to test the exchangeability assumption.
+        If X and XTXinv are passed, x must be the last row of X.
+        '''
+        # Inner method to compute the p-value from NC scores
+        def calc_p(A, B, y):
+            # Nonconformity scores are A + yB = y - yhat
+            Alpha = A + y*B
+            alpha_y = Alpha[-1]
+            lt = np.where(Alpha < alpha_y)[0].shape[0]
+            eq = np.where(Alpha == alpha_y)[0].shape[0]
+            tau = self.rnd_gen.uniform(0, 1)
+            p_y = (lt + tau * eq)/Alpha.shape[0]
+            return p_y
+        
+        if precomputed is not None:
+            A = precomputed['A']
+            B = precomputed['B']
+            if A is not None and B is not None:
+                p_y = calc_p(A, B, y)
+            else:
+                X = precomputed['X']
+                if X.shape[0] > 0:
+                    assert np.allclose(x, X[-1])
+                    D = precomputed['D']
+                    A, B = self.compute_A_and_B(D, self.y, self.k)
+                    p_y = calc_p(A, B, y)
+                else:
+                    p_y = self.rnd_gen.uniform(0, 1)
+        else:
+            if self.X is not None:
+                d = self.distance_func(self.X, x)
+                D = self.update_distance_matrix(self.D, d)
+                A, B = self.compute_A_and_B(D, self.y, self.k)
+                p_y = calc_p(A, B, y)
+            else:
+                p_y = self.rnd_gen.uniform(0, 1)
+        return p_y
+    
+
 if __name__ == "__main__":
     import doctest
     import sys
     (failures, _) = doctest.testmod()
     if failures:
         sys.exit(1)
+

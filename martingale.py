3ca86a98d105e409abe3b847db6ff99709dd0faf
Johan Hallberg Szabadvary <johan.hallberg@algorithma.se>
2024-07-12 11:49:40 +0200

    Added offline calculation

--- martingale.py
+++ martingale.py
@@ -36,20 +36,22 @@ class PluginMartingale:
         def get_density_estimate(p_values):
             P = np.array([[-p, p, 2-p] for p in p_values]).flatten()[:, np.newaxis]
             if len(P) == 0:
-                return None
+                return None, None
             kde = KernelDensity(kernel='gaussian', bandwidth='silverman').fit(P)
-            return kde
-        def betting_function(p, d):
+            func = lambda p :np.exp(kde.score_samples([[p]])[0])
+            norm_fac = quad(func, 0, 1)
+            return kde, norm_fac
+        
+        def betting_function(p, d, norm_fac):
             if not d: 
                 return 1
             if 0 <= p <= 1:
-                func = lambda p :np.exp(d.score_samples([[p]])[0])
-                norm_fac = quad(func, 0, 1)
                 return np.exp(d.score_samples([[p]])[0]) / norm_fac[0]
             else:
                 return 0.0
-        d = get_density_estimate(p_values[:-1])
-        return betting_function(p_values[-1], d)
+            
+        d, norm_fac = get_density_estimate(p_values[:-1])
+        return betting_function(p_values[-1], d, norm_fac)
     
     def update_log_martingale(self, p_values):
         if self.betting_function == 'kernel':
@@ -68,6 +70,28 @@ class PluginMartingale:
     def M(self):
         return np.exp(self.logM)
     
+
+    def offline_martingale(self, p_values):
+        '''
+        Compute martingale offline, given a sequence of p-values
+        '''
+        if self.betting_function == 'kernel':
+            log_betting_function = lambda p_values: np.log(self.kernel_density_betting_function(p_values))
+        else:
+            raise NotImplementedError('Currently only kernel betting function is available. More to come...')
+        p_values = np.array(p_values).flatten() # Could be a list or a vector, so make it into a vector either way
+        logM = 0.0
+        logM_arr = np.zeros((p_values.shape[0] + 1, ))
+        logM_arr[0] = logM
+        for i, _ in enumerate(p_values):
+            logM += log_betting_function(p_values[:i+1])
+            logM_arr[i+1] = logM
+        
+        M_arr = np.exp(logM_arr)
+        max = M_arr.max()
+        M = np.exp(logM)
+        return max, M, M_arr
+
 if __name__ == "__main__":
     import doctest
     import sys

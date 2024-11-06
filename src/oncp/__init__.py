668bc37a9b9d07a4d4fe7c48dafda95b484d762b
Johan Hallberg Szabadvary <johan.hallberg@algorithma.se>
2024-11-06 12:22:03 +0100

    add standard exports

--- src/oncp/__init__.py
+++ src/oncp/__init__.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python
 
-from .CRR import ConformalRidgeRegressor
+from .CRR import ConformalRidgeRegressor, ConformalNearestNeighboursRegressor
+from .classifiers import ConformalNearestNeighbours # TODO: Classifier?
+from .martingale import PluginMartingale

651df08ea618a9124588add2a2516674cf5925bc
Johan Hallberg Szabadvary <johan.hallberg@algorithma.se>
2025-01-22 13:33:59 +0100

    Add RidgePredictionMachine to __init__.py

--- src/online_cp/__init__.py
+++ src/online_cp/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
-from .regressors import ConformalRidgeRegressor, ConformalNearestNeighboursRegressor
+from .regressors import ConformalRidgeRegressor
 from .classifiers import ConformalNearestNeighboursClassifier 
 from .martingale import PluginMartingale
+from .CPS import RidgePredictionMachine

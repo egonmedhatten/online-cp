8a9abfa2c215f3185bac6666718fe7900a889087
Johan Hallberg Szabadvary <johan.hallberg@algorithma.se>
2025-02-12 15:58:27 +0100

    Remove print

--- src/online_cp/__init__.py
+++ src/online_cp/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-print("Initializing __init__.py")
 from online_cp.regressors import ConformalRidgeRegressor
 from online_cp.classifiers import ConformalNearestNeighboursClassifier
 from online_cp.martingale import PluginMartingale

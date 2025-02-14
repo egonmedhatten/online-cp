7c4126678813cb13a4e002af9b9c637035397ceb
Johan Hallberg Szabadvary <johan.hallberg@algorithma.se>
2025-02-14 13:22:52 +0100

    Remove unnecessary stuff

--- sync_with_SU.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -rlptzv --progress --exclude=.git . joha5534@shell3.math.su.se:/home/joha5534/Documents/online-cp
--- sync_with_desktop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -rlptzv --progress --exclude=.git . johan@192.168.1.130:/home/johan/Documents/Phd/online-cp

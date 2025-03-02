0239d8b045238603157bce228e6a370ac8a30ab2
Johan Hallberg Szabadvary <johan.hallberg@algorithma.se>
2025-03-02 13:17:22 +0100

    Import evaluation

--- src/online_cp/__init__.py
+++ src/online_cp/__init__.py
@@ -4,3 +4,4 @@ from online_cp.regressors import ConformalRidgeRegressor
 from online_cp.classifiers import ConformalNearestNeighboursClassifier
 from online_cp.martingale import PluginMartingale
 from online_cp.CPS import RidgePredictionMachine
+from online_cp.evaluation import Evaluation, Err, OE, OF, WinklerScore, Width, CRPS
